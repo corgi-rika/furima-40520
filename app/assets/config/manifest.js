672c0d8e75092c7f4abcc6e371835c452fd40cc3
corgi-rika <scaricky3132@gmail.com>
2024-06-25 16:50:42 +0900

    ルートパスを設定してアイテム一覧を表示

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,8 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link application.css
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link application.css
