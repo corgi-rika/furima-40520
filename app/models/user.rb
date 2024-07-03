389e410c25308d9fe6d049824cc861a2659195f5
corgi-rika <scaricky3132@gmail.com>
2024-07-04 00:55:30 +0900

    新規登録/ユーザー情報の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #  新規登録/ユーザー情報
+  validates :nickname, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
