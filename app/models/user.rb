c9f3c2d67fe82d35fde0feb71746ede9d6b4938e
corgi-rika <scaricky3132@gmail.com>
2024-07-06 00:46:58 +0900

    新規登録/本人情報確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,11 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
+
+  # 新規登録/本人情報確認
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/ }
+  validates :birth_date, presence: true
 end
