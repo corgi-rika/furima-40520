a586dbda8ad784aa5819576bd865bbb65cd1032e
corgi-rika <scaricky3132@gmail.com>
2024-07-08 00:24:16 +0900

    実装済みの案内用文の削除とテストコードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #  新規登録/ユーザー情報
   validates :nickname, presence: true
-  validates :password, length: { minimum: 6 }
   validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
-
-  # 新規登録/本人情報確認
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/ }
