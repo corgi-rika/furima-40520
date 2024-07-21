21e138a26138e5cd6e1dc167afc58458b8db482d
corgi-rika <scaricky3132@gmail.com>
2024-07-21 15:20:34 +0900

    editアクションとbefore_action :set_itemの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  # before_action :set_item, only: [:show, :edit, :update]
+  # before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -18,13 +20,23 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   # def destroy
   #   @item = Item.find(params[:id])
   #   @item.destroy
   #   redirect_to root_path
+  # end
+
+  def edit
+  end
+
+  # def update
+  #   if @item.update(item_params)
+  #     redirect_to item_path(@item)
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
   # end
 
   private
@@ -34,5 +46,13 @@ class ItemsController < ApplicationController
       :name, :description, :category_id, :condition_id, :shipping_fee_id,
       :prefecture_id, :shipping_day_id, :price, :image
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user == @item.user
   end
 end
