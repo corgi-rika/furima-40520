0e2ffbb3c795d5cc18aaf6811e41b3710f10da72
corgi-rika <scaricky3132@gmail.com>
2024-08-05 22:17:56 +0900

    クレジットカード購入機能の追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,33 +3,45 @@ class OrdersController < ApplicationController
   before_action :set_params
   before_action :redirect_if_sold_out, only: [:index, :create]
 
-  # def index
-  #   @purchase_shipping_info = PurchaseShippingInfo.new
-  # end
+  def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+    @purchase_shipping_info = PurchaseShippingInfo.new
+  end
 
-  # def create
-  #   @purchase_shipping_info = PurchaseShippingInfo.new(purchase_shipping_info_params)
-  #   if @purchase_shipping_info.valid?
-  #     @purchase_shipping_info.save
-  #     redirect_to root_path
-  #   else
-  #     render :index, status: :unprocessable_entity
-  #   end
-  # end
+  def create
+    @purchase_shipping_info = PurchaseShippingInfo.new(purchase_shipping_info_params)
+    if @purchase_shipping_info.valid?
+      pay_item
+      @purchase_shipping_info.save
+      redirect_to root_path
+    else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render :index, status: :unprocessable_entity
+    end
+  end
 
-  # private
+  private
 
-  # def set_params
-  #   @item = Item.find(params[:item_id])
-  # end
+  def set_params
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_shipping_info_params[:token],
+      currency: 'jpy'
+    )
+  end
 
   def redirect_if_sold_out
     redirect_to root_path if @item.sold_out?
   end
 
-  # def purchase_shipping_info_params
-  #   params.require(:purchase_shipping_info).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(
-  #     user_id: current_user.id, item_id: params[:item_id]
-  #   )
-  # end
+  def purchase_shipping_info_params
+    params.require(:purchase_shipping_info).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
 end
